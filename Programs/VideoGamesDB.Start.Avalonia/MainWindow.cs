9b775c629c2c0c7fc55b8c8a86107f51107a1317
Gary Hertel <32446542+garyhertel@users.noreply.github.com>
2025-06-03 22:03:04 -0700

    Updating Versions

--- Programs/VideoGamesDB.Start.Avalonia/MainWindow.cs
+++ Programs/VideoGamesDB.Start.Avalonia/MainWindow.cs
@@ -27,6 +27,6 @@ public class MainWindow : BaseWindow
 		Name = "VideoGamesDB",
 		LinkType = "VideoGamesDB",
 		Version = ProjectSettings.ProgramVersion(),
-		DataVersion = new Version(0, 2),
+		DataVersion = new Version(0, 9, 8),
 	};
 }
