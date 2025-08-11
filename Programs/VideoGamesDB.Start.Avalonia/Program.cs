677c4f098b6f6afafe504c3c64be89ee9e4aafe7
Gary Hertel <32446542+garyhertel@users.noreply.github.com>
2025-08-10 22:06:47 -0700

    Restoring default DataValidators now that there's another workaround

--- Programs/VideoGamesDB.Start.Avalonia/Program.cs
+++ Programs/VideoGamesDB.Start.Avalonia/Program.cs
@@ -1,5 +1,4 @@
 using Avalonia;
-using Avalonia.Data.Core.Plugins;
 
 namespace VideoGamesDB.Start.Avalonia;
 
@@ -7,12 +6,6 @@ class Program
 {
 	static int Main(string[] args)
 	{
-		// Remove Default DataAnnotations Validators
-		// These validators show before values are entered, which ends up showing too many initial warnings
-		// https://docs.avaloniaui.net/docs/data-binding/data-validation
-		// Add custom template?
-		BindingPlugins.DataValidators.RemoveAt(0);
-
 		AppBuilder builder = BuildAvaloniaApp();
 
 		return builder.StartWithClassicDesktopLifetime(args);
